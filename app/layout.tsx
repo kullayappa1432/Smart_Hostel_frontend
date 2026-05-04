9019c5c7b4e96b81adf8134bcffdc77bb09e2c08
Rallapalli Kullayappa <rallapallikullayappakullayappa@gmail.com>
2026-05-04 18:16:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5586933340157941"
-     crossorigin="anonymous"></script>
-      </head>
       <body>
         <ErrorBoundary>
           {children}
